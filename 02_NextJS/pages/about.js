81543772308fe8e930d795502795ec24b62ae243
Armando Rivera <rivera.armando997@gmail.com>
2021-01-28 13:53:04 -0600

    02-Enlazando páginas

--- 02_NextJS/pages/about.js
+++ 02_NextJS/pages/about.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
 
 const About = () => {
   return (
     <div>
+      <Navbar/>
       Esta es la página de about
     </div>
   )
