cd439e7f9f7bbd00231dbbc81a0f8a651fac8df5
Armando Rivera <rivera.armando997@gmail.com>
2021-01-27 23:54:28 -0600

    01-SearchFilter

--- 01_ReactJS/01-platzi-badges/src/components/BadgesList.js
+++ 01_ReactJS/01-platzi-badges/src/components/BadgesList.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 import Gravatar from './Gravatar';
 import './styles/BadgeList.css';
 
-export class BadgesList extends Component {
-  render() {
-    if (this.props.badges.length === 0) {
-      return (
-        <div>
-          <h3>Ups parece que aún no hay ningún badge creado</h3>
-          <Link className="btn btn-primary" to="/badges/new">Crea un nuevo Badge</Link>
-        </div>
-      )
-    }
+function useSearchBadges(badges) {
+  const [ query, setQuery ] = React.useState('');
+  const [ filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter(badge => {
+      return `${badge.firstName.toLowerCase()} ${badge.lastName.toLowerCase()}`.includes(query.toLowerCase());
+    });
+    setFilteredBadges(result);
+  }, [badges, query]);
+
+  return [query, setQuery, filteredBadges];
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+  const [query, setQuery, filteredBadges] = useSearchBadges(badges); 
+  if (filteredBadges.length === 0) {
     return (
+      <div>
+        <div className="form-group">
+          <label>Filter Badges</label>
+          <input type="text" className="form-control"
+          value={query}
+          onChange={(e) => {setQuery(e.target.value)}}/>
+        </div>
+        <h3>Ups parece que aún no hay ningún badge creado</h3>
+        <Link className="btn btn-primary" to="/badges/new">Crea un nuevo Badge</Link>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input type="text" className="form-control"
+        value={query}
+        onChange={(e) => {setQuery(e.target.value)}}/>
+      </div>
+
       <ul className="list-unstyled">
-        {this.props.badges.map((badge) => {
+        {filteredBadges.map((badge) => {
           // El key es importante para React ya que lo tomara como identificador
           // Para saber en si el elemento cambia de estado
           return (
@@ -32,8 +61,8 @@ export class BadgesList extends Component {
           )
         })}
       </ul>
-    );
-  }
+    </div>
+  );
 }
 
 export default BadgesList;
