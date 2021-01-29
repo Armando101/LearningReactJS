3c8e54ce34e8a9ed270cc49e16c4f82a6bf55836
Armando Rivera <rivera.armando997@gmail.com>
2021-01-29 17:07:46 -0600

    02-Next; Concectar el endpoint de obtener solo un item

---
+++ 02_NextJS/pages/product/[id].tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
+import Navbar from '../../components/Navbar/Navbar';
+
+const ProductItem = () => {
+  // const router = useRouter();
+  const { query: { id } } = useRouter();
+  const [productList, setProductList] = useState<TProduct>({});
+
+  useEffect(() => {
+    window.fetch(`/api/avo/${id}`)
+      .then(response => response.json())
+      .then((data) => {
+        setProductList(data);
+      });
+  });
+
+  return (
+    <div>
+      <Navbar />
+      Este es el producto: {productList?.name}
+    </div>
+  )
+}
+
+export default ProductItem
