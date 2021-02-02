0024e4ccb244b201d9a4c32e4e6e4e7700079a22
Armando Rivera <rivera.armando997@gmail.com>
2021-02-01 19:28:42 -0600

    02-NextJS; Alias en Next

--- 02_NextJS/pages/product/[id].tsx
+++ 02_NextJS/pages/product/[id].tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '@components/Navbar/Navbar';
 
 const ProductItem = () => {
   // const router = useRouter();
