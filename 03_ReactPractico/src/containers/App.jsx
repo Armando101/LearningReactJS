edacaf9978cacaae17775db5e39f8b2fcfa84dd1
Armando Rivera <rivera.armando997@gmail.com>
2021-02-12 15:59:15 -0600

    34_ReactPractico; React Hooks

--- 03_ReactPractico/src/containers/App.jsx
+++ 03_ReactPractico/src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -9,6 +9,16 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
+  const [ videos, setVideos ] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
   return (
     <React.Fragment>
       <Header/>
