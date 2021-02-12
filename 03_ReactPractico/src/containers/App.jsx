6c8b5aca0f6d4e92ffda1c118fca6441f30d7115
Armando Rivera <rivera.armando997@gmail.com>
2021-02-11 20:04:42 -0600

    34_ReactPractico; Estructura del header

---
+++ 03_ReactPractico/src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header';
+
+const App = () => {
+  return (
+    <React.Fragment className="App">
+      <Header/>
+    </React.Fragment>
+  )
+}
+
+export default App
