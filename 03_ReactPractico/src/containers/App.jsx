fa5f40686c648159db45722b6ee458c0c9d93d54
Armando Rivera <rivera.armando997@gmail.com>
2021-02-12 19:53:23 -0600

    03_ReactPractico; PropTypes

--- 03_ReactPractico/src/containers/App.jsx
+++ 03_ReactPractico/src/containers/App.jsx
@@ -5,47 +5,48 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
+
 const API = 'http://localhost:3000/initalState';
-
 const App = () => {
-  const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: [] });
 
-  useEffect(() => {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => setVideos(data));
-  }, []);
-
-  console.log('videos', videos);
+  const initialState = useInitialState(API);
 
   return (
     <React.Fragment>
       <Header/>
       <Search/>
-      {videos.mylist.length > 0 &&
+      {initialState.mylist.length > 0 &&
         <Categories title="Mi lista">
           <Carousel>
-            <CarouselItem/>
+            {initialState.mylist.map(item => {
+              return <CarouselItem key={item.id} {...item}/>
+            })}
           </Carousel>
         </Categories>
       }
       
-      <Categories title="tendencias">
-        <Carousel>
-          {videos.trends.map(item => {
-            return <CarouselItem key={item.id} {...item}/>
-          })}
-        </Carousel>
-      </Categories> 
-
-      <Categories title="Originales">
-        <Carousel>
-          <CarouselItem/>
-        </Carousel>
-      </Categories>
+      {initialState.trends.length > 0 &&
+        <Categories title="Tendencias">
+          <Carousel>
+            {initialState.trends.map(item => {
+              return <CarouselItem key={item.id} {...item}/>
+            })}
+          </Carousel>
+        </Categories> 
+      }
       
+      {initialState.originals.length > 0 &&
+        <Categories title="Originales">
+          <Carousel>
+            {initialState.originals.map(item => {
+              return <CarouselItem key={item.id} {...item}/>
+            })}
+          </Carousel>
+        </Categories>
+      }
+
       <Footer/>
     </React.Fragment>
   )
