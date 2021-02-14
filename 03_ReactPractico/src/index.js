e9a7c51e77604d1446b857fa021c8d2de1ce9f2e
Armando Rivera <rivera.armando997@gmail.com>
2021-02-13 23:19:58 -0600

    34_RouterRedux; Instalación de redux y encapsular con Provider

--- 03_ReactPractico/src/index.js
+++ 03_ReactPractico/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDom.render(<App />, document.getElementById('app'));
+// Se tiene que encapsular nuestra aplicación dentro de un provider
+// Porque nada fuera del provider podrá acceder al store
+// El <Provider /> hace que la store de Redux esté disponible para cualquier componente anidado que se haya incluido en la función connect().
+
+ReactDom.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
