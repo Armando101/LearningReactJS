dbbae86763913313a2c8845f7c0b97001c2b7bed
Armando Rivera <rivera.armando997@gmail.com>
2021-02-13 18:14:33 -0600

    04_RouterRedux; Login y historyApiFallback

--- 03_ReactPractico/src/containers/Home.jsx
+++ 03_ReactPractico/src/containers/Home.jsx
@@ -9,7 +9,7 @@ import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
-const App = () => {
+const Home = () => {
 
   const initialState = useInitialState(API);
 
@@ -52,4 +52,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Home;
