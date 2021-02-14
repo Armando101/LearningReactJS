967db7c21ef3df3b3c296097ea0638e5b8667998
Armando Rivera <rivera.armando997@gmail.com>
2021-02-13 23:52:54 -0600

    04_RouterRedux; Creando store y función connect

--- 03_ReactPractico/src/containers/Home.jsx
+++ 03_ReactPractico/src/containers/Home.jsx
@@ -1,41 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { connect } from "react-redux";
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initalState';
-const Home = () => {
-  const initialState = useInitialState(API);
+const Home = ({ myList, trends, originals}) => {
   return (
     <>
       <Search/>
-      {initialState.mylist.length > 0 &&
+      {myList.length > 0 &&
         <Categories title="Mi lista">
           <Carousel>
-            {initialState.mylist.map(item => {
+            {myList.map(item => {
               return <CarouselItem key={item.id} {...item}/>
             })}
           </Carousel>
         </Categories>
       }
       
-      {initialState.trends.length > 0 &&
+      {trends.length > 0 &&
         <Categories title="Tendencias">
           <Carousel>
-            {initialState.trends.map(item => {
+            {trends.map(item => {
               return <CarouselItem key={item.id} {...item}/>
             })}
           </Carousel>
         </Categories> 
       }
       
-      {initialState.originals.length > 0 &&
+      {originals.length > 0 &&
         <Categories title="Originales">
           <Carousel>
-            {initialState.originals.map(item => {
+            {originals.map(item => {
               return <CarouselItem key={item.id} {...item}/>
             })}
           </Carousel>
@@ -45,4 +43,22 @@ const Home = () => {
   )
 }
 
-export default Home;
+// Traemos los datos que necesitamos del estado
+const mapStateToProps = state => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals
+  }
+}
+
+// Indicamos que el componente Home va a recibir como promiedades lo que me retorna mapSatateToProps
+// En este caso las acciones van a ser null
+// La función connecto nos permite conectar el componente con el state
+// Para conectar un componente a Redux vamos a necesitar importar connect de react-redux
+// connect va a aceptar dos parámetros:
+// mapStateToProps: es una función que le va a indicar al provider qué información necesitamos del store.
+// mapDispatchToProps: es un objeto con las distintas funciones para ejecutar una action en Redux.
+
+// export default connect(props, actions)(Home);
+export default connect(mapStateToProps, null)(Home);
