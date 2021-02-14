dbf8cadc0f818c239138e580e39ed795a01c47db
Armando Rivera <rivera.armando997@gmail.com>
2021-02-14 00:42:10 -0600

    04_RouterRedux; Crear reducers, mapDispatchToProps y disparar la acción

--- 03_ReactPractico/src/components/CarouselItem.jsx
+++ 03_ReactPractico/src/components/CarouselItem.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => {
+const CarouselItem = (props) => {
+  const { id, cover, title, year, contentRating, duration } = props;
+  const handleSetFavorite = () => {
+    // Ejecuta la función setFavorite y automáticamente la dispara el reducer
+    props.setFavorite({
+      id, cover, title, year, contentRating, duration
+    })
+  }
   return (
       <div className="carousel-item">
         <img className="carousel-item__img" src={cover} alt={title}  />
         <div className="carousel-item__details">
           <div>
-            <img className="carousel-item__details--img" src={playIcon} alt="Play Icon"/> 
-            <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon"/> 
+            <img className="carousel-item__details--img"
+              src={playIcon}
+              alt="Play Icon"/>
+            <img className="carousel-item__details--img"
+            src={plusIcon}
+            alt="Plus Icon" 
+            onClick={handleSetFavorite}/> 
           </div>
           <p className="carousel-item__details--title">{title}</p>
           <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
@@ -29,4 +43,10 @@ CarouselItem.propTypes = {
   duration: PropTypes.number
 }
 
-export default CarouselItem
+const mapDispatchToProps = {
+  setFavorite,
+}
+
+// Le digo a React que voy a utilizar la acción de setFavorite y que al momento de ejecutar la función la dispare
+// Por lo tanto necesito que está función esté disponible en las propiedades
+export default connect(null, mapDispatchToProps)(CarouselItem);
