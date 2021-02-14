2a845a2d3e4005ca8cf362270142a7a7e200b8d8
Armando Rivera <rivera.armando997@gmail.com>
2021-02-13 18:25:07 -0600

    04_ReactRouter; NotFound and Fragments

---
+++ 03_ReactPractico/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>No encontrado</h1>
+    </>
+  )
+}
+
+export default NotFound
--- 03_ReactPractico/src/routes/App.js
+++ 03_ReactPractico/src/routes/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NotFound from '../components/NotFound';
 import Register from '../components/Register';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
@@ -10,6 +11,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/" component={Login} />
       <Route exact path="/" component={Register} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
