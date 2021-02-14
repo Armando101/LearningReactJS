85d105bd9c1cfe88d90b32caca48ab03ce0176a5
Armando Rivera <rivera.armando997@gmail.com>
2021-02-13 18:50:34 -0600

    04_RouterRedux; Layout

--- 03_ReactPractico/src/containers/Home.jsx
+++ 03_ReactPractico/src/containers/Home.jsx
@@ -1,21 +1,16 @@
-import React, { useState , useEffect} from 'react'
-import Header from '../components/Header';
+import React from 'react'
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 const Home = () => {
-
   const initialState = useInitialState(API);
-
   return (
-    <React.Fragment>
-      <Header/>
+    <>
       <Search/>
       {initialState.mylist.length > 0 &&
         <Categories title="Mi lista">
@@ -46,9 +41,7 @@ const Home = () => {
           </Carousel>
         </Categories>
       }
-
-      <Footer/>
-    </React.Fragment>
+    </>
   )
 }
 
--- 03_ReactPractico/src/routes/App.js
+++ 03_ReactPractico/src/routes/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import NotFound from '../components/NotFound';
-import Register from '../components/Register';
+import NotFound from '../containers/NotFound';
+import Register from '../containers/Register';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Layout from '../components/Layout';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/" component={Login} />
-      <Route exact path="/" component={Register} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
