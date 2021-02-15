dc4eeea9f7b5198cc1b9b1bcbe6066ea388551f5
Armando Rivera <rivera.armando997@gmail.com>
2021-02-15 15:34:03 -0600

    04_RouterRedux; Construir el componente player. Incluir el link al video en los items del carousel

--- 03_ReactPractico/src/routes/App.js
+++ 03_ReactPractico/src/routes/App.js
@@ -5,6 +5,7 @@ import Register from '../containers/Register';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Layout from '../components/Layout';
+import Player from '../containers/Player';
 
 const App = () => (
   <BrowserRouter>
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/player/:id" component={Player} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
