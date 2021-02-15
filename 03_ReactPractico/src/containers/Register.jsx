300b2ca3e2b6f8a3fd7dda4adee7f5599c824d1f
Armando Rivera <rivera.armando997@gmail.com>
2021-02-15 15:16:20 -0600

    04_RouterRedux; Manejo del estado en register

--- 03_ReactPractico/src/containers/Register.jsx
+++ 03_ReactPractico/src/containers/Register.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
 const Register = () => {
+  const [form, setForm] = useState({
+    email: '',
+    name: '',
+    password: ''
+  });
+
+  const handleInput = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
   <section className="register">
     <section className="register__container">
       <h2>Regístrate</h2>
-      <form className="register__container--form">
-        <input className="input" type="text" placeholder="Nombre"/>
-        <input className="input" type="text" placeholder="Correo"/>
-        <input className="input" type="password" placeholder="Contraseña"/>
+      <form className="register__container--form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Nombre"
+          name="name"
+          onChange={handleInput}
+        />
+        <input
+          className="input"
+          type="text"
+          placeholder="Correo"
+          name="email"
+          onChange={handleInput}
+        />
+        <input
+          className="input"
+          type="password"
+          placeholder="Contraseña"
+          name="password"
+          onChange={handleInput}
+        />
         <button className="button">Registrarme</button>
       </form>
       <Link to="/login">Iniciar sesión</Link>
