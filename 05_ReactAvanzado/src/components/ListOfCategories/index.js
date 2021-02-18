2f26f3d376aeda8aed8e0bf4f015a9d995c1a939
Armando Rivera <rivera.armando997@gmail.com>
2021-02-18 15:29:18 -0600

    05_ReactAvanzo; Use state y user effect, petición ✈️

--- 05_ReactAvanzado/src/components/ListOfCategories/index.js
+++ 05_ReactAvanzado/src/components/ListOfCategories/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 import { Item, List } from './styles';
-import { categories } from '@api/db.json';
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://petgram-server-edsf8xpy2.now.sh/categories')
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+    // El array vacío como segundo parámetro significa que sólo se va a ejecutar al montar el componente
+    // Este arreglo pude terner configuraciones de cuándo se tiene que ejecutar
+  }, []);
+
   return (
     <List>
       {categories.map((category) => {
