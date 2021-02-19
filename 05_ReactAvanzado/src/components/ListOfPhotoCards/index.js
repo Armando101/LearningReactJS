bc1297daee18e56a587255535196ad332337de0f
Armando Rivera <rivera.armando997@gmail.com>
2021-02-19 14:14:06 -0600

    05_ReactAvanzado; Instalación y peticiones con Graphql 🍇

--- 05_ReactAvanzado/src/components/ListOfPhotoCards/index.js
+++ 05_ReactAvanzado/src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import { PhotoCard } from '../PhotoCard';
 
+const getPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(getPhotos);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <p>Error</p>;
+
   return (
     <ul>
-      {[0, 1, 2, 3].map((id) => (
-        <PhotoCard key={id} id={id} />
+      {data.photos.map((photoCard, id) => (
+        <PhotoCard key={id} {...photoCard} />
       ))}
     </ul>
   );
