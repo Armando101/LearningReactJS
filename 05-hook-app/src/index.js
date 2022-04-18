b55dcbcf4edf4a9746bca5a000c24f9b66e8cbc6
Armando Rivera <rivera.armando997@gmail.com>
2022-04-18 14:42:53 -0500

    setState hook

---
+++ 05-hook-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CounterApp } from "./components/01-useState/CounterApp";
+// import { HookApp } from "./HookApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<CounterApp />);
