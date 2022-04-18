b3c283c36adf08e56b23fd0f831cdca79d43e90e
Armando Rivera <rivera.armando997@gmail.com>
2022-04-18 14:58:39 -0500

    Custom hook useCounter

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CounterApp } from "./components/01-useState/CounterApp";
+import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+// import { CounterApp } from "./components/01-useState/CounterApp";
 // import { HookApp } from "./HookApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CounterApp />);
+root.render(<CounterWithCustomHook />);
