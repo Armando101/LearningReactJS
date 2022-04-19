c45661cbe30fa57b1aa3d49bafff724e1a916537
Armando Rivera <rivera.armando997@gmail.com>
2022-04-18 23:38:25 -0500

    useForm custom Hook

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { SimpleForm } from "./components/02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { HookApp } from "./HookApp";
+// import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SimpleForm />);
+root.render(<FormWithCustomHook />);
