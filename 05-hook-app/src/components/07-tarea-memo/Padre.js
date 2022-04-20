ecf75400f9302a9a1179725bd02facde82ad0cc7
Armando Rivera <rivera.armando997@gmail.com>
2022-04-20 14:45:56 -0500

    useCallback and React.memo

---
+++ 05-hook-app/src/components/07-tarea-memo/Padre.js
@@ -0,0 +1,28 @@
+import { Hijo } from "./Hijo";
+import { useCallback, useState } from "react";
+import "../05-useLayoutEffect/effects.css";
+
+export const Padre = () => {
+  const numeros = [2, 4, 6, 8, 10];
+  const [valor, setValor] = useState(0);
+
+  const incrementar = useCallback(
+    (num) => {
+      setValor((e) => e + num);
+    },
+    [setValor]
+  );
+
+  return (
+    <div>
+      <h1>Padre</h1>
+      <p> Total: {valor} </p>
+
+      <hr />
+
+      {numeros.map((n) => (
+        <Hijo key={n} numero={n} incrementar={incrementar} />
+      ))}
+    </div>
+  );
+};
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { CallbackHook } from "./components/06-memos/CallbackHook";
+import { Padre } from "./components/07-tarea-memo/Padre";
 
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 // import { CounterApp } from "./components/01-useState/CounterApp";
@@ -12,6 +12,7 @@ import { CallbackHook } from "./components/06-memos/CallbackHook";
 // import { Layout } from "./components/05-useLayoutEffect/Layout";
 // import { Memorize } from "./components/06-memos/Memorize";
 // import { MemoHook } from "./components/06-memos/MemoHook";
+// import { CallbackHook } from "./components/06-memos/CallbackHook";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CallbackHook />);
+root.render(<Padre />);
