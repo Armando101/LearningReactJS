e41bb9775d31727399fa8e23293d3c136f1e1425
Armando Rivera <rivera.armando997@gmail.com>
2022-04-23 00:58:09 -0500

    useContext save user data in the context

--- 05-hook-app/src/components/09-useContext/HomeScreen.js
+++ 05-hook-app/src/components/09-useContext/HomeScreen.js
@@ -2,13 +2,13 @@ import { useContext } from "react";
 import { UserContext } from "./UserContext";
 
 export const HomeScreen = () => {
-  const userContext = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
   return (
     <div>
       <h1>Home Screen</h1>
       <hr />
-      <h4>User: {userContext.name}</h4>
+      <pre>{JSON.stringify(user, null, 3)}</pre>
     </div>
   );
 };
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,14 +1,12 @@
+import { useState } from "react";
 import { AppRouter } from "./AppRouter";
 import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
-  const user = {
-    id: 1234,
-    name: "Armando",
-    email: "armando@rivera.com",
-  };
+  const [user, setUser] = useState({});
+
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={{ user, setUser }}>
       <AppRouter />
     </UserContext.Provider>
   );
